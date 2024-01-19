0eccadc535ec137e83ab9ac45d6c22f53f65f935
y-eonee <leenayeon@sookmyung.ac.kr>
2024-01-19 20:11:55 +0900

    Revert "로컬에 올려보기 실험/스크립트작업"
    This reverts commit 6a5dbaa1d36345fb658900f4ac5354d9725c073a.

--- 2023SoluxProject/Assets/NY/Script/PlayerCamera.cs
+++ 2023SoluxProject/Assets/NY/Script/PlayerCamera.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public Transform target;
-
-    public float smoothSpeed = 3;
-    public Vector2 offset;
-    public float limitMinX, limitMaxX, limitMinY, limitMaxY;
-    float cameraHalfWidth, cameraHalfHeight;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        cameraHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
-        cameraHalfHeight = Camera.main.orthographicSize;
+        
     }
 
-    private void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 desiredPosition = new Vector3(
-            Mathf.Clamp(target.position.x + offset.x, limitMinX + cameraHalfWidth, limitMaxX - cameraHalfWidth),   // X
-            Mathf.Clamp(target.position.y + offset.y, limitMinY + cameraHalfHeight, limitMaxY - cameraHalfHeight), // Y
-            -10);                                                                                                  // Z
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+        
     }
 }
