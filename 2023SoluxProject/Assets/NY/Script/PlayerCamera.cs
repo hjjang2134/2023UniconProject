0c43c77fe4980bff0eb8c38ed117455cfc2668da
y-eonee <leenayeon@sookmyung.ac.kr>
2024-01-19 21:36:19 +0900

    스크립트 작업 중/다음부턴 제브랜치에서하겟슴다..

--- 2023SoluxProject/Assets/NY/Script/PlayerCamera.cs
+++ 2023SoluxProject/Assets/NY/Script/PlayerCamera.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+
+    public float smoothSpeed = 3;
+    public Vector2 offset;
+    public float limitMinX, limitMaxX, limitMinY, limitMaxY;
+    float cameraHalfWidth, cameraHalfHeight;
+
+    private void Start()
     {
-        
+        cameraHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+        cameraHalfHeight = Camera.main.orthographicSize;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 desiredPosition = new Vector3(
+            Mathf.Clamp(target.position.x + offset.x, limitMinX + cameraHalfWidth, limitMaxX - cameraHalfWidth),   // X
+            Mathf.Clamp(target.position.y + offset.y, limitMinY + cameraHalfHeight, limitMaxY - cameraHalfHeight), // Y
+            -10);                                                                                                  // Z
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
     }
 }
