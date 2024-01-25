8ba2aa85d0f5187e8b57b9e4f7d000b694cf700a
Anjihee <jihee5100@sookmyung.ac.kr>
2024-01-25 10:36:12 +0900

    map3 수정중

--- 2023SoluxProject/Assets/JH/Scripts/Change.cs
+++ 2023SoluxProject/Assets/JH/Scripts/Change.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Change : MonoBehaviour
 {
-    public void SceneChange(){
+    public void SceneChangeop(){
         SceneManager.LoadScene("2_Opening");
+    }
+
+    public void SceneChangemap1(){
+        SceneManager.LoadScene("4_GameHJ");
+    }
+
+    public void SceneChangemap2(){
+        SceneManager.LoadScene("5_GameNY");
+    }
+
+    public void SceneChangemap3(){
+        SceneManager.LoadScene("6_GameYJ");
     }
 }
